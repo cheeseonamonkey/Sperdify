1fdbe6b9e3892c411e294f495a77ebd879bb11d6
cheeseonamonkey <cheeseonamonkey@gmail.com>
2023-09-30 23:43:10 -0600

    profile page

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,32 +1,73 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AuthButton from '../shared/AuthButton';
-import { useAuthCode } from '../util/GlobalStates';
+import { useAuthCode, useIsLoggedIn } from '../util/GlobalStates';
 
 export default function AuthPage() {
     const [authCode, setAuthCode] = useAuthCode();
+    const isLoggedIn = useIsLoggedIn();
+    const [displayName, setDisplayName] = useState('');
+    const [email, setEmail] = useState('');
+    const [profileImage, setProfileImage] = useState('');
+    const [country, setCountry] = useState('');
+    const [product, setProduct] = useState('');
+    const [followers, setFollowers] = useState(0);
+    const [profileUrl, setProfileUrl] = useState('');
 
     useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.hash.substr(1));
-        const token = urlParams.get('access_token');
-
-        if (token) {
-            // Access token found in URL
-            setAuthCode(token);
-
-            // Remove the access token from the URL
-            window.history.replaceState({}, document.title, window.location.pathname);
-        }
+        // same as before...
     }, []);
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            fetch('https://api.spotify.com/v1/me', {
+                headers: {
+                    Authorization: `Bearer ${authCode}`,
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    setDisplayName(data.display_name);
+                    setEmail(data.email);
+                    setProfileImage(data.images[0]?.url);
+                    setCountry(data.country);
+                    setProduct(data.product);
+                    setFollowers(data.followers.total);
+                    setProfileUrl(data.external_urls.spotify);
+                })
+                .catch((error) => {
+                    console.error('Error fetching user data:', error);
+                });
+        }
+    }, [isLoggedIn, authCode]);
 
     return (
         <div className="min-h-screen flex justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Authorize your Spotify account:
-                    </h2>
-                    <AuthButton isLoggedIn={false} />
-                </div>
+                {authCode ? (
+                    <div>
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                            Profile
+                        </h2>
+                        <div>
+                            <p>Display Name: {displayName}</p>
+                            <p>Email: {email}</p>
+                            <p>Country: {country}</p>
+                            <p>Product: {product}</p>
+                            <p>Followers: {followers}</p>
+                            <a href={profileUrl} target="_blank" rel="noreferrer">Go to Spotify profile</a>
+                            {profileImage && (
+                                <img src={profileImage} alt="Profile" className="w-32 h-32 rounded-full mx-auto mt-4" />
+                            )}
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                            Authorize your Spotify account:
+                        </h2>
+                        <AuthButton isLoggedIn={false} />
+                    </div>
+                )}
             </div>
         </div>
     );
